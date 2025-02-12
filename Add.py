dd3e6e5884815a8b63f4b90ef488caf78d86d22a
Urvashi-Patel99 <urvashipatel87935@gmail.com>
2025-02-12 20:27:05 +0530

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,7 @@
+import arithmetic
+x=arithmetic.Add(10,20)
+print(x)
+y=arithmetic.Multiply(10,20)
+print(y)
+z=arithmetic.division(20,10)
+print(z)
---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+def Add(x,y):
+     return x+y
+
+def Multiply(a,b):
+      return a*b
+def division(a,b):
+      return a/b
